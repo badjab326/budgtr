0f5ad97547aba829f855ec030a3df4b367f0b00b
“julian <badjab326@gmail.com>
2022-05-18 15:15:51 -0700

    Added content to index and show pages

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require('express');
-const budget = require('./models/budget');
-
-// initialize express application
+const res = require('express/lib/response');
+const Budget = require('./models/budget');
 const app = express();
-
-// configure application settings
 const port = 3000;
 
+//Mount Middleware
+app.use(express.static('public'))
 
+//Index
+app.get('/budgets', (req, res) => {
+    res.render('index.ejs', { Budget });
+});
+
+//New
+app.get("/budgets/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+//Create
+app.post('/budgets', (re1, res) => {
+    res.redirect("index.ejs");
+});
+
+//Show
+app.get('/budgets/:id', (req, res) => {
+    res.render("show.ejs", {
+        budgets: Budget[req.params.id]
+    });
+});
+
+//default test
+app.get('/', (req, res) => {
+    res.send('Budgtr is online');
+});
 
 app.listen(port, () =>{
-    console.log(`Listening on port`, port)
+    console.log(`Listening on port`, port);
 });
